12ca0e3381b34b737bc6f627e5b9885fe6308059
Alijon0185 <muxtorovalijon0127@gmail.com>
2023-07-08 12:21:53 +0500

    create order details

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\OrderDetailsController;
 
 
 /*
@@ -22,10 +23,17 @@ use App\Http\Controllers\OrderController;
 Route::post('/login', [AuthController::class, 'Login']);
 Route::group(['middleware' => ['auth:sanctum']], function () {
 
-    Route::group(['prefix' => 'order'], function () {
-        Route::get('/search/taxi', [OrderController::class, 'searchTaxi']);
-        Route::get('/show', [OrderController::class, 'orderShow']);
+    Route::group(['prefix' => 'client'], function () {
+        Route::group(['prefix' => 'order'], function () {
+            Route::get('/search/taxi', [OrderController::class, 'searchTaxi']);
+            Route::get('/show', [OrderController::class, 'orderShow']);
+        });
+        Route::group(['prefix' => 'orderDetail'], function () {
+            Route::post('/store', [OrderDetailsController::class, 'store']);
+            // Route::get('/show', [OrderController::class, 'orderShow']);
+        });
     });
+    
     Route::post('/logout', [AuthController::class, 'Logout']);
 });
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
