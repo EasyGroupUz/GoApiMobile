12628d2429820145b9549e880c48029c60515996
itkey0808 <54538502+Elyor941004@users.noreply.github.com>
2023-08-08 18:31:58 +0500

    offercontroller

--- app/Http/Controllers/OfferController.php
+++ app/Http/Controllers/OfferController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Offer;
 use App\Models\Driver;
+use App\Models\OrderDetail;
+use App\Models\Order;
 use App\Models\Client;
 use Illuminate\Http\Request;
 
@@ -12,18 +14,32 @@ class OfferController extends Controller
 
     public function postOffer(Request $request){
         $field = $request->validate([
-            'driver_id'=>'required|integer',
-            'client_id'=>'required|integer',
             'order_id'=>'required|integer',
             'order_detail_id'=>'required|integer',
             'price'=>'required|integer',
             'comment'=>'required|string',
         ]);
         $offer = new Offer();
-        $offer->driver_id = $field['driver_id'];
-        $offer->client_id = $field['client_id'];
-        $offer->order_id = $field['order_id'];
-        $offer->order_detail_id = $field['order_detail_id'];
+        $order_detail = OrderDetail::find($field['order_detail_id']);
+        $order = Order::find($field['order_id']);
+        if(!isset($order_detail)){
+            $response = [
+                'status'=>false,
+                'message'=>'Order detail not found'
+            ];
+            return response()->json($response);
+        }
+        if(!isset($order)){
+            $response = [
+                'status'=>false,
+                'message'=>'Order not found'
+            ];
+            return response()->json($response);
+        }
+        $offer->driver_id = $order->driver_id;
+        $offer->client_id = $order_detail->client_id;
+        $offer->order_id = $order->id;
+        $offer->order_detail_id = $order_detail->id;
         $offer->price = $field['price'];
         $offer->status = 1;
         $offer->comment = $field['comment'];
