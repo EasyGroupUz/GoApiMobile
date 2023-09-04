3f0a9cdcd522376e5fb1b52e5292bb362fb08995
itkey0808 <54538502+Elyor941004@users.noreply.github.com>
2023-09-04 16:44:08 +0500

    Верификация исправлено

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -199,17 +199,10 @@ class AuthController extends Controller
                     }else{
                         $new_user->personal_account = 1000000;
                     }
-                    if(!isset($user->personal_info_id)){
-                        $personal_info = new PersonalInfo();
-                        $personal_info->phone_number = (int)$fields['phone_number'];
-                        $personal_info->save();
-                        $user->personal_info_id = $personal_info->id;
-                    }else{
-                        $personal_info = PersonalInfo::withTrashed()->find($user->personal_info_id);
-                        if(isset($personal_info->deleted_at)){
-                            $personal_info->deleted_at = NULL;
-                        }
-                    }
+                    $personal_info = new PersonalInfo();
+                    $personal_info->phone_number = (int)$fields['phone_number'];
+                    $personal_info->save();
+                    $new_user->personal_info_id = $personal_info->id;
                     $personal_info->phone_number = (int)$fields['phone_number'];
                     $personal_info->save();
                     $new_user->personal_info_id = $personal_info->id;
@@ -232,12 +225,13 @@ class AuthController extends Controller
                     }
                     $user->email = $model->phone_number;
 
-                    $personal_info = PersonalInfo::withTrashed()->find($user->personal_info_id);
-                    if(!isset($personal_info->id)){
+                    if(!isset($user->personal_info_id)){
                         $personal_info = new PersonalInfo();
                         $personal_info->phone_number = (int)$fields['phone_number'];
+                        $personal_info->save();
                         $user->personal_info_id = $personal_info->id;
                     }else{
+                        $personal_info = PersonalInfo::withTrashed()->find($user->personal_info_id);
                         if(isset($personal_info->deleted_at)){
                             $personal_info->deleted_at = NULL;
                         }
