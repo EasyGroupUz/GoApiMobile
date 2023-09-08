5bc4ae05f60705fba4b7f069894aeb76af1d7889
itkey0808 <54538502+Elyor941004@users.noreply.github.com>
2023-09-08 18:48:39 +0500

    Cors test

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 define('LARAVEL_START', microtime(true));
 
 $allowedOrigins = array(
-    'http://api.easygo.uz/',
+    'http://api.easygo.uz',
     'http://localhost:3000'
 );
 if(isset($_SERVER['HTTP_ORIGIN']) && $_SERVER['HTTP_ORIGIN'] != ''){
