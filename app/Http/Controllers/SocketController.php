85ca90a819ffe276ab96318e3270e91d46f0468d
Alijon0185 <muxtorovalijon0127@gmail.com>
2023-09-12 14:24:29 +0500

    testing

--- app/Http/Controllers/SocketController.php
+++ app/Http/Controllers/SocketController.php
@@ -52,7 +52,7 @@ class SocketController extends Controller implements MessageComponentInterface
             $user_from_id=$data['user_from_id'];
             $user_to_id=$data['user_to_id'];
 
-            // $order = Order::find($data['order_id']);
+            $order = Order::find($data['order_id']);
             // $id=$order->id;
 
             $from->send(json_encode($data['order_id']));
