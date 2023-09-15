428bd697d53f663f9e41c4cc4eff7174baea4bd2
IbrohimEasy <Ibrohimeasy@gmail.com>
2023-09-15 17:53:39 +0500

    приведение к int везде, где используется атрибут «doc_status»

--- app/Http/Controllers/OrderDetailsController.php
+++ app/Http/Controllers/OrderDetailsController.php
@@ -506,7 +506,7 @@ class OrderDetailsController extends Controller
             ->leftJoin('yy_class_lists as class', 'class.id', '=', 'car.class_list_id')
             ->leftJoin('yy_statuses as status', 'status.id', '=', 'or.status_id')
             ->where('od.client_id',auth()->id())
-            ->select('or.id', 'od.id as order_detail_id', 'or.start_date', 'or.price', 'of.status as offer_status', 'or.seats as seats_count', 'or.booking_place as booking_count', 'usC.id as client_id', 'piC.last_name as c_last_name', 'piC.first_name as c_first_name', 'piC.middle_name as c_middle_name', 'piC.phone_number as c_phone_number', 'piC.avatar as c_avatar', 'usC.rating as c_rating', 'pi.last_name', 'pi.first_name', 'pi.middle_name', 'pi.phone_number', 'pi.avatar as dImg', 'us.rating', 'car.id as car_id', 'cl.name as car_name', 'col.name as color_name', 'col.code as color_code', 'car.production_date', 'class.name as class_name', 'car.reg_certificate', 'car.reg_certificate_image', 'car.images as car_images', 'or.options', 'from.name as from', 'from.lng as from_lng', 'from.lat as from_lat', 'to.name as to', 'to.lng as to_lng', 'to.lat as to_lat', 'status.name as status_name', 'us.id as driver_id', 'dr.id as dr_id')
+            ->select('or.id', 'od.id as order_detail_id', 'or.start_date', 'or.price', 'of.status as offer_status', 'or.seats as seats_count', 'or.booking_place as booking_count', 'usC.id as client_id', 'piC.last_name as c_last_name', 'piC.first_name as c_first_name', 'piC.middle_name as c_middle_name', 'piC.phone_number as c_phone_number', 'piC.avatar as c_avatar', 'usC.rating as c_rating', 'pi.last_name', 'pi.first_name', 'pi.middle_name', 'pi.phone_number', 'pi.avatar as dImg', 'us.rating', 'car.id as car_id', 'cl.name as car_name', 'col.name as color_name', 'col.code as color_code', 'car.production_date', 'class.name as class_name', 'car.reg_certificate', 'car.reg_certificate_image', 'car.images as car_images', 'or.options', 'from.name as from', 'from.lng as from_lng', 'from.lat as from_lat', 'to.name as to', 'to.lng as to_lng', 'to.lat as to_lat', 'status.name as status_name', 'us.id as driver_id', 'dr.id as dr_id', 'dr.doc_status as driver_doc_status')
             ->get();
 
         return $offers;
@@ -558,7 +558,7 @@ class OrderDetailsController extends Controller
                 'phone_number' => $offer->phone_number,
                 'img' => ($offer->dImg) ? asset('storage/avatar/' . $offer->dImg) : '',
                 'rating' => $offer->rating,
-                'doc_status' => ($offer->dr_id) ? true : false
+                'doc_status' => $offer->driver_doc_status
             ];
             $arr[$n]['car'] = [
                 'id' => $offer->car_id,
