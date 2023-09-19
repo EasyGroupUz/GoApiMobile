982cadea8e0068d29652a099f4e553b787998293
IbrohimEasy <Ibrohimeasy@gmail.com>
2023-09-19 16:59:27 +0500

    Тест уведомлений

--- app/Http/Controllers/SocketController.php
+++ app/Http/Controllers/SocketController.php
@@ -171,7 +171,7 @@ class SocketController extends Controller implements MessageComponentInterface
                     $userSend = User::find($user_to_id);
                     // $userSender = User::find($user_from_id);
                     
-                    $device = ($userSend) ? json_decode($userSend->device_type) : [];
+                    $device = ($userSend) ? json_decode($userSend->device_id) : [];
                     $title = translate_api("You've got mail", $userSend->language);
                     $message = $text;
                     $largeIcon = ($userSend && $userSend->personalInfo && ($userSend->personalInfo->avatar != NULL)) ? asset('storage/user/' . $userSend->personalInfo->avatar) : '';
