ea7da2bf2324815ba19dd0efd8c95d279227e7b5
Alijon0185 <muxtorovalijon0127@gmail.com>
2023-09-22 16:22:22 +0500

    added information for chat store

--- app/Http/Controllers/SocketController.php
+++ app/Http/Controllers/SocketController.php
@@ -434,16 +434,20 @@ class SocketController extends Controller implements MessageComponentInterface
     {
         // $data=$request->all();
         $chat_id=$request->chat_id;
+        $order_id=$request->order_id;
+        $user_from_id=$request->user_$user_from_id;
+        $user_to_id=$request->user$user_to_id;
 
             $new_chat = [
-                'id' =>$chat_id,
-                'order_detail_id' =>$chat_id
-                // 'user_to_id' =>null,
-                // 'text' => null
+                'id' => $chat_id,
+                'user_from_id' => $user_from_id,
+                'user_to_id' => $user_to_id,
+                'order_id' => $order_id,
+                'order_detail_id' => $chat_id
             ];
 
             $new_chat = Chat::create($new_chat);
-            return $this->success('new chat created', 200,);
+            return $this->success('new chat created', 200);
 
 
 
