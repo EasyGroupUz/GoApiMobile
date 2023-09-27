a4cb73e496d6fe95847b53ecfa5f7771778ef3a7
IbrohimEasy <Ibrohimeasy@gmail.com>
2023-09-27 15:16:09 +0500

    the notification when booking has changed

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -1349,7 +1349,7 @@ class OrderController extends Controller
     
     
                         $device = ($order->driver) ? json_decode($order->driver->device_id) : [];
-                        $title = 'Your request has been accepted';
+                        $title = 'Reserved a place on your trip';
                         $message = ': ' . (($order && $order->from) ? $order->from->name : '') . ' - ' . (($order && $order->to) ? $order->to->name : '');
                         $user_id = ($order->driver) ? $order->driver->id : 0;
                         $entity_id = $order->id;
@@ -1392,7 +1392,7 @@ class OrderController extends Controller
             $saveOrderDetail = $orderDetail->save();
 
             $device = ($order->driver) ? json_decode($order->driver->device_id) : [];
-            $title = 'Your request has been accepted';
+            $title = 'Reserved a place on your trip';
             $message = ': ' . (($order && $order->from) ? $order->from->name : '') . ' - ' . (($order && $order->to) ? $order->to->name : '');
             $user_id = ($order->driver) ? $order->driver->id : 0;
             $entity_id = $order->id;
